40f5c5edecb40db3b9b7d26ba0798bcb2895e8bd
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-10 12:32:45 +0400

    /*добавила отсупы*/

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
+const btn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav");
 
-const btn = document.querySelector('.menu-btn');
-const nav = document.querySelector('.nav');
-
-btn.addEventListener('click', () => { //добавляться и удаляться класс при  клике
-nav.classList.toggle('menu-open');
-})
+btn.addEventListener("click", () => {
+  //добавляться и удаляться класс при  клике
+  nav.classList.toggle("menu-open"); //при клике на btn к nav добавится класс и toggle при первом клике добавится при втором исчезнет
+});
